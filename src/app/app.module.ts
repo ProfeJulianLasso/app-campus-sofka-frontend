eeb2cd6edd8817fa8ea8951896db4fd35e1233ec
Julian Lasso <98534497+ProfeJulianLasso@users.noreply.github.com>
2022-08-03 18:16:55 -0500

    Se agrega tema personalizado para Angular Material y se comienza la organización de la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+// Libraries
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Routes
+import { AppRoutingModule } from './app-routing.module';
+
+// Template
+import { AppComponent } from './templates/main2022/main/app.component';
 
 @NgModule({
   declarations: [AppComponent],
