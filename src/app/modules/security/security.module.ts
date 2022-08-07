d896d63083a4e0ee49d5ac0649873d5233de6452
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-08-06 20:57:33 -0500

    Configurando el diseño

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -6,6 +6,9 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 // Routing
 import { SecurityRoutingModule } from './security-routing.module';
@@ -25,9 +28,7 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input'
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     LoginComponent,
@@ -46,7 +47,7 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatIconModule
+    MatIconModule,
 
   ],
   exports: [LoginModalComponent]
