ca704bc40547c89c4984b96544db567c8480fe27
yeferson1221 <alejandro.yandd@gmail.com>
2022-08-06 20:56:12 -0500

    solucion conflictos de rutas

--- src/app/modules/security/security-routing.module.ts
+++ src/app/modules/security/security-routing.module.ts
@@ -10,36 +10,39 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { PasswordRecoveryComponent } from './pages/password-recovery/password-recovery.component';
 
 const routes: Routes = [
-
   {
     path: '',
     component: AuthComponent,
     children: [
       {
         path: 'login',
-        component: LoginComponent,
+        component: LoginComponent
       },
       {
         path: 'singin',
-        component: SignInComponent,
+        component: SignInComponent
       },
       {
         path: 'singup',
-        component: SignUpComponent,
+        component: SignUpComponent
+      },
+      {
+        path: 'password',
+        component: PasswordRecoveryComponent
       },
       {
         path: '**',
-        redirectTo: 'singup'
+        redirectTo: 'password'
       }
     ]
   }
-
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class SecurityRoutingModule { }
+export class SecurityRoutingModule {}
