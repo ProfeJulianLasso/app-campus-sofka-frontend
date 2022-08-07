6035e93683015eefcd09c96069d0bcdaf4b1de6f
yeferson1221 <alejandro.yandd@gmail.com>
2022-08-06 21:08:23 -0500

    comflicto solucionado

--- src/app/modules/security/pages/password-recovery/password-recovery.component.ts
+++ src/app/modules/security/pages/password-recovery/password-recovery.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
+
 import { FirebaseCodeErrorService } from '../../services/firebase-code-error.service';
 @Component({
   selector: 'sofka-password-recovery',
@@ -16,12 +16,12 @@ export class PasswordRecoveryComponent {
   constructor(
     private fb: FormBuilder,
     private afAuth: AngularFireAuth,
-    private toastr: ToastrService,
+   
     private router: Router,
     private firebaseError: FirebaseCodeErrorService
   ) {
     this.recoveryUser = this.fb.group({
-      correo: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]]
     });
   }
 
@@ -31,15 +31,11 @@ export class PasswordRecoveryComponent {
     this.afAuth
       .sendPasswordResetEmail(email)
       .then(() => {
-        this.toastr.info(
-          'Le·enviamos·un·correo·para·restablecer·su·password',
-          'Recuperar·Password'
-        );
         this.router.navigate(['/login']);
       })
       .catch((error) => {
         this.loading = false;
-        this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
+        this.firebaseError.codeError(error.code), 'Error';
       });
   }
 }
