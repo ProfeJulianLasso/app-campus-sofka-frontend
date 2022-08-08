9b79db7bc0a9fabda86c685944609743017d6a49
yeferson1221 <alejandro.yandd@gmail.com>
2022-08-08 14:09:25 -0500

    se agrega la interfase de Course ademas justes de deocumentacion

--- src/app/modules/course/pages/course/course.component.ts
+++ src/app/modules/course/pages/course/course.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../../../templates/main2022/services/dashboard.service';
+import { DashboardComponent } from '../../../../templates/main2022/components/dashboard/dashboard.component';
 
 @Component({
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
-  constructor() {}
+  constructor(private dashboardService: DashboardService) {}
 
   ngOnInit(): void {}
+
+  courseAll(){
+    
+  }
+
 }
---
+++ src/app/modules/security/interfaces/ICourse.ts
@@ -0,0 +1,12 @@
+export interface Course {
+  uid: string;
+  levels: string;
+  course: string;
+  category: string;
+  subCategory: string;
+  trophy: string;
+  trophyActivated: boolean;
+  completed: number;
+  photoURL: string;
+  image: boolean;
+}
