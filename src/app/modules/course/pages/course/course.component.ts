d2cb28072daf8c50e3daaafe634d99771e3f7028
yeferson1221 <alejandro.yandd@gmail.com>
2022-08-08 14:17:14 -0500

    metodo listar coursos terminado

--- src/app/modules/course/pages/course/course.component.ts
+++ src/app/modules/course/pages/course/course.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../../../templates/main2022/services/dashboard.service';
-import { DashboardComponent } from '../../../../templates/main2022/components/dashboard/dashboard.component';
+import { Course } from '../../../security/interfaces/ICourse';
 
 @Component({
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  listCourse: Course[] = [];
+
   constructor(private dashboardService: DashboardService) {}
 
-  ngOnInit(): void {}
-
-  courseAll(){
-    
+  ngOnInit(): void {
+    this.courseAll();
   }
 
+  courseAll() {
+    this.dashboardService.getCourses().subscribe(
+      (data) => {
+        this.listCourse = data;
+        console.log(this.listCourse);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
